9b3bbae0aa288ff7bf2c4473abb3a9369e5f3fe0
Parthiv Reddy Pelluru <ParthivReddy-2005>
2025-06-10 21:13:33 +0530

    commiting the files to push into github

---
+++ app.js
@@ -0,0 +1,18 @@
+const heading = document.createElement("h1");
+heading.innerHTML ="hello world from javascript";
+const root1 = document.getElementById("root1");
+root1.appendChild(heading);
+
+const parent = React.createElement("div",
+    {id:"parent"},
+    [React.createElement("div",
+        {id:"child1"},
+    [React.createElement("h1",{},"I'm h1 tag"),
+    React.createElement("h2",{},"I'm h2 tag")]),
+    React.createElement("div",
+        {id:"child2"},
+    [React.createElement("h1",{},"I'm h1 tag"),
+    React.createElement("h2",{},"I'm h2 tag")])]
+    );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
